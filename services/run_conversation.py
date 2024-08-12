d07e69c242cfdd5ed15c321630d92f1170ac0591
bky373 <bo373@naver.com>
2024-08-12 21:51:44 +0900

    Update function calling

--- services/run_conversation.py
+++ services/run_conversation.py
@@ -1,39 +1,40 @@
-from openai import OpenAI # openai==1.2.0
 import json
- 
-client = OpenAI(api_key="YOU_API_KEY", base_url="https://api.upstage.ai/v1/solar")
- 
-from tools.function_calls import functions as FUNCTIONS
-from tools.function_calls import functions_description as FUNCTIONS_DESCRIPTION
+import os
 
-def run_conversation():
-    # Step 1: send the conversation and available functions to the model
+import dotenv
+from openai import OpenAI  # openai==1.2.0
+
+dotenv.load_dotenv()
+client = OpenAI(api_key=os.getenv("API_KEY"), base_url="https://api.upstage.ai/v1/solar")
+
+from tools.function_calls import functions as FUNCTIONS
+from tools.function_calls import function_descriptions as FUNCTION_DESCRIPTIONS
+
+
+def run_conversation(message):
     messages = [
         {
             "role": "user",
-            "content": "How is the weather in San Francisco today?",
+            "content": message,
         }
     ]
-    tools = FUNCTIONS_DESCRIPTION
+    tools = FUNCTION_DESCRIPTIONS
     response = client.chat.completions.create(
         model="solar-1-mini-chat",
         messages=messages,
         tools=tools,
-        tool_choice="auto",  # auto is default, but we'll be explicit
+        tool_choice="auto",
     )
     response_message = response.choices[0].message
     tool_calls = response_message.tool_calls
- 
-    # Step 2: check if the model wanted to call a function
+    print(f'tool_calls: {tool_calls}')
+
     if tool_calls:
-        # Step 3: call the function
-        # Note: the JSON response may not always be valid; be sure to handle errors
-        available_functions = FUNCTIONS
-        messages.append(response_message)  # extend conversation with assistant's reply
-        # Step 4: send the info for each function call and function response to the model
+        messages.append(response_message)
+
         for tool_call in tool_calls:
             function_name = tool_call.function.name
-            function_to_call = available_functions[function_name]
+            function_to_call = FUNCTIONS[function_name]
             function_args = json.loads(tool_call.function.arguments)
             function_response = function_to_call(
                 location=function_args.get("location"),
@@ -46,12 +47,12 @@ def run_conversation():
                     "name": function_name,
                     "content": function_response,
                 }
-            )  # extend conversation with function response
-        second_response = client.chat.completions.create(
+            )
+        print(f'messages: {messages}')
+        return client.chat.completions.create(
             model="solar-1-mini-chat",
             messages=messages,
-        )  # get a new response from the model where it can see the function response
-        return second_response
- 
- 
-print(run_conversation())
+        )
+
+
+print(run_conversation("Hi, Please recommend a place to eat near east-kareum"))
